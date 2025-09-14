6e4c909f569e195be4d396b3f9ac06b5d6f15eed
[dyad] <joaopedrob837@icloud.com>
2025-09-14 20:51:23 -0300

    [dyad] Removendo o logo do sistema - wrote 3 file(s), deleted 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center">
-            <img src="/logo.png" alt="Logo" className="h-10" />
+            
           </Link>
 
           {/* Desktop Nav */}
